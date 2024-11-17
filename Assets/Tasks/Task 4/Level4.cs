953549416456c0dcd249e944e9aa4e62a0ea06bd
IntactoGit <intacto1996@gmail.com>
2024-11-17 17:42:43 +0300

    initial commit and push

---
+++ Assets/Tasks/Task 4/Level4.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class Level4 : MonoBehaviour
+{
+
+    public bool UseGravity = false;
+    private float _speed = 5;
+
+    
+    private void Update()
+    {
+
+        var step = Time.deltaTime * _speed;
+        var transformPosition = transform.position;
+        transformPosition.x += step;
+
+        transform.position = transformPosition;
+    }
+    
+    
+
+}
